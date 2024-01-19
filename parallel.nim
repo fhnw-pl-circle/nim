0e52893516d7172119782f7a879d2f7c077c9733
Simon Freiermuth <simon.freiermuth1@students.fhnw.ch>
2024-01-19 07:36:33 +0000

    shared heap

--- parallel.nim
+++ parallel.nim
@@ -1,6 +1,6 @@
 import threadpool
 import math
-import std/os
+import std/[os, locks, strutils]
 {.experimental: "parallel".}
 
 # BASICS
@@ -42,6 +42,29 @@ block noSharedHeap:
     # var thread: Thread[void]
     # createThread(thread, showData)
 
+    # communication via channels
+    # 1:1 communication (not 1:many)
+    var
+        channel: Channel[string] # actually there still seams to be a process wide shared heap https://nim-lang.org/docs/system.html#example-passing-channels-safely
+        sender: Thread[void]
+        receiver: Thread[void]
+        receiver2: Thread[void]
+
+    channel.open(10) # max amount of items in channel
+    createThread receiver, proc(){.thread.} = 
+        var msg = channel.recv()
+        echo "received: ", msg
+        msg = channel.recv()
+        echo "received: ", msg
+    
+    createThread sender, proc(){.thread.} = 
+        sleep(500)
+        for w in "hello from other channel".split(' '):
+            channel.send(w)
+
+    joinThreads(receiver, receiver2, sender)
+
+
 # THREADPOOL
 block threadpool:
     # same example as above but using a threadpool
@@ -76,11 +99,42 @@ block threadpool:
 
     var values : array[1000, int]
 
+    # from sequential to parallel
+    # for i in 0..values.high:
+    #     values[i] = longRunningCalculation(values[i])
+
     parallel: # there are some limitations what can be done inside this block
         for i in 0..values.high:
             values[i] = spawn longRunningCalculation(values[i])
     
-    # for i in 0..values.high:
-    #     values[i] = longRunningCalculation(values[i])
-
     echo sum(values)
+
+# SHARED HEAP
+# Since Nim 2 there is some confusion: since the new default memory management strategy
+# "ORC" seams to have a shared heap: https://nim-lang.org/docs/mm.html
+block sharedHeap:
+    # running this file with the old GC "--mm:markAndSweep" will segfault
+    # But running it with new orc and arc will work fine
+    type 
+        RefBox = ref object
+            data: int
+
+    var
+        r = RefBox()
+        s: string = "hello"
+        p: ptr string = s.addr()
+
+        t1: Thread[void]
+        t2: Thread[void]
+
+    createThread(t1, proc() {.thread.} =
+        p[].add(" world")
+        # will still not work (not GC safe):
+        # r[].data = 100
+    )
+    joinThread(t1)
+    createThread(t2, proc() {.thread.} =
+        echo "t2\t", p[]
+    )
+    joinThread(t2)
+    echo "t0\t", s
